41a8f245dd383c157819ccee878dd709548e70f5
carash <wibisanawidirga@gmail.com>
2018-04-24 19:33:16 +0700

    patch 1.01

--- js/claw.js
+++ js/claw.js
@@ -85,96 +85,105 @@ var worldMat = function() {
 	return mat
 }
 
+var calcMat = function() {
+	var mat = translate(0, 0, 0);
+	var par = [this.offsetMat];
+	var curr = this.parent;
+
+	while (curr) {
+		par.push(curr.offsetMat);
+		curr = curr.parent;
+	}
+
+	par.reverse()
+	par.forEach(function(m) {
+		mat = mult(mat, m);
+	});
+
+	return mat
+}
+
 var clawMachine = {}
 
 clawMachine.upperArm = {
 	parent: null,
-	offset: [0.0, -10.0, 0.0],
-	offsetMat: translate(0.0, -10.0, 0.0),
-	width: 1.5,
+	defaultMat: mult(translate(0.0, 5.0, 0.0), rotate(180, 0, 0, 90)),
+	offsetMat: mult(translate(0.0, 5.0, 0.0), rotate(180, 0, 0, 90)),
+	width: 1.0,
 	height: 3.0,
-	calcPos: worldPos,
-	defaultMat: worldMat,
+	calculateMat: calcMat,
 };
 
 clawMachine.lowerArm = {
 	parent: clawMachine.upperArm,
-	offset: [0.0, 3.0, 0.0],
-	offsetMat: translate(0.0, 3.0, 0.0),
-	width: 1.0,
-	height: 4.0,
-	calcPos: worldPos,
-	defaultMat: worldMat,
+	defaultMat: translate(0.0, 1.5, 0.0),
+	offsetMat: translate(0.0, 1.5, 0.0),
+	width: 0.7,
+	height: 5.5,
+	calculateMat: calcMat,
 };
 
 clawMachine.clawBase = {
 	parent: clawMachine.lowerArm,
-	offset: [0.0, 4.0, 0.0],
+	defaultMat: translate(0.0, 4.0, 0.0),
 	offsetMat: translate(0.0, 4.0, 0.0),
-	width: 2.7,
+	width: 2.3,
 	height: 1.0,
-	calcPos: worldPos,
-	defaultMat: worldMat,
+	calculateMat: calcMat,
 };
 
 clawMachine.upperClaw1 = {
 	parent: clawMachine.clawBase,
-	offset: [1.0, 1.0, 0.0],
-	offsetMat: mult(translate(1.0, 1.0, 0.0), rotate(-30, 0, 0, 90)),
+	defaultMat: mult(translate(0.6, 1.0, 0.0), rotate(-45, 0, 0, 90)),
+	offsetMat: mult(translate(0.6, 1.0, 0.0), rotate(-45, 0, 0, 90)),
 	width: 0.4,
 	height: 1.5,
-	calcPos: worldPos,
-	defaultMat: worldMat,
+	calculateMat: calcMat,
 };
 
 clawMachine.lowerClaw1 = {
 	parent: clawMachine.upperClaw1,
-	offset: [0.0, 1.5, 0.0],
+	defaultMat: mult(translate(0.0, 1.5, 0.0), rotate(60, 0, 0, 90)),
 	offsetMat: mult(translate(0.0, 1.5, 0.0), rotate(60, 0, 0, 90)),
 	width: 0.3,
 	height: 1.5,
-	calcPos: worldPos,
-	defaultMat: worldMat,
+	calculateMat: calcMat,
 };
 
 clawMachine.upperClaw2 = {
 	parent: clawMachine.clawBase,
-	offset: [-0.5, 1.0, 0.866],
-	offsetMat: mult(mult(translate(-0.5, 1.0, 0.866), rotate(-120, 0, 90, 0)), rotate(-30, 0, 0, 90)),
+	defaultMat: mult(mult(translate(-0.3, 1.0, 0.5), rotate(-120, 0, 90, 0)), rotate(-45, 0, 0, 90)),
+	offsetMat: mult(mult(translate(-0.3, 1.0, 0.5), rotate(-120, 0, 90, 0)), rotate(-45, 0, 0, 90)),
 	width: 0.4,
 	height: 1.5,
-	calcPos: worldPos,
-	defaultMat: worldMat,
+	calculateMat: calcMat,
 };
 
 clawMachine.lowerClaw2 = {
 	parent: clawMachine.upperClaw2,
-	offset: [0.0, 1.5, 0.0],
+	defaultMat: mult(translate(0.0, 1.5, 0.0), rotate(60, 0, 0, 90)),
 	offsetMat: mult(translate(0.0, 1.5, 0.0), rotate(60, 0, 0, 90)),
 	width: 0.3,
 	height: 1.5,
-	calcPos: worldPos,
-	defaultMat: worldMat,
+	calculateMat: calcMat,
 };
 
 clawMachine.upperClaw3 = {
 	parent: clawMachine.clawBase,
-	offset: [-0.5, 1.0, -0.866],
-	offsetMat: mult(mult(translate(-0.5, 1.0, -0.866), rotate(120, 0, 90, 0)), rotate(-30, 0, 0, 90)),
+	defaultMat: mult(mult(translate(-0.3, 1.0, -0.5), rotate(120, 0, 90, 0)), rotate(-45, 0, 0, 90)),
+	offsetMat: mult(mult(translate(-0.3, 1.0, -0.5), rotate(120, 0, 90, 0)), rotate(-45, 0, 0, 90)),
 	width: 0.4,
 	height: 1.5,
-	calcPos: worldPos,
-	defaultMat: worldMat,
+	calculateMat: calcMat,
 };
 
 clawMachine.lowerClaw3 = {
 	parent: clawMachine.upperClaw3,
-	offset: [0.0, 1.5, 0.0],
+	defaultMat: mult(translate(0.0, 1.5, 0.0), rotate(60, 0, 0, 90)),
 	offsetMat: mult(translate(0.0, 1.5, 0.0), rotate(60, 0, 0, 90)),
 	width: 0.3,
 	height: 1.5,
-	calcPos: worldPos,
-	defaultMat: worldMat,
+	calculateMat: calcMat,
 };
 
 var renderObjects = [
@@ -188,24 +197,6 @@ var renderObjects = [
 	clawMachine.upperClaw3,
 	clawMachine.lowerClaw3,
 ];
-
-var horse = {
-	render: renderHorse,
-	base: {
-		width: 5.0,
-		height: 2.0
-	},
-	upperArm: {
-		width: 0.5,
-		height: 5.0
-	},
-	lowerArm: {
-		height: 5.0,
-		width: 0.5
-	},
-	pos: [0.0, 0.0, 0.0],
-	rot: [0.0, 0.0, 0.0]
-};
 
 // Parameters controlling the size of the Robot's arm
 
@@ -280,10 +271,6 @@ function colorCube() {
 	quad(4, 5, 6, 7);
 	quad(5, 4, 0, 1);
 }
-
-//____________________________________________
-
-// Remmove when scale in MV.js supports scale matrices
 
 function scale4(a, b, c) {
 	var result = mat4();
@@ -398,7 +385,7 @@ window.onload = function init() {
 var display = function() {
 	requestAnimFrame(display);
 	var radios = document.getElementsByName("model");
-	renderHorse();
+	renderClaw();
 };
 
 // ----------------------------------------------------------------------------
@@ -406,61 +393,6 @@ var display = function() {
 function drawComponent(comp) {
 	var s = scale4(comp.width, comp.height, comp.width);
 	var instanceMatrix = mult(translate(0.0, 0.5 * comp.height, 0.0), s);
-
-	var t = mult(modelViewMatrix, instanceMatrix);
-	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
-	gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
-}
-
-// ----------------------------------------------------------------------------
-
-function drawUpperArm() {
-	var s = scale4(clawMachine.upperArm.width, clawMachine.upperArm.height, clawMachine.upperArm.width);
-	var instanceMatrix = mult(translate(0.0, 0.5 * clawMachine.upperArm.height, 0.0), s);
-
-	var t = mult(modelViewMatrix, instanceMatrix);
-	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
-	gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
-}
-
-// ----------------------------------------------------------------------------
-
-function drawLowerArm() {
-	var s = scale4(clawMachine.lowerArm.width, clawMachine.lowerArm.height, clawMachine.lowerArm.width);
-	var instanceMatrix = mult(translate(0.0, 0.5 * clawMachine.lowerArm.height, 0.0), s);
-
-	var t = mult(modelViewMatrix, instanceMatrix);
-	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
-	gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
-}
-
-// ----------------------------------------------------------------------------
-
-function drawClawBase() {
-	var s = scale4(clawMachine.clawBase.width, clawMachine.clawBase.height, clawMachine.clawBase.width);
-	var instanceMatrix = mult(translate(0.0, 0.5 * clawMachine.clawBase.height, 0.0), s);
-
-	var t = mult(modelViewMatrix, instanceMatrix);
-	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
-	gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
-}
-
-// ----------------------------------------------------------------------------
-
-function drawUpperClaw() {
-	var s = scale4(clawMachine.upperClaw1.width, clawMachine.upperClaw1.height, clawMachine.upperClaw1.width);
-	var instanceMatrix = mult(translate(0.0, 0.5 * clawMachine.upperClaw1.height, 0.0), s);
-
-	var t = mult(modelViewMatrix, instanceMatrix);
-	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
-	gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
-}
-
-// ----------------------------------------------------------------------------
-
-function drawLowerClaw() {
-	var s = scale4(clawMachine.lowerClaw1.width, clawMachine.lowerClaw1.height, clawMachine.lowerClaw1.width);
-	var instanceMatrix = mult(translate(0.0, 0.5 * clawMachine.lowerClaw1.height, 0.0), s);
 
 	var t = mult(modelViewMatrix, instanceMatrix);
 	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
@@ -479,26 +411,6 @@ function base() {
 
 // ----------------------------------------------------------------------------
 
-function upperArm() {
-	var s = scale4(UPPER_ARM_WIDTH, UPPER_ARM_HEIGHT, UPPER_ARM_WIDTH);
-	var instanceMatrix = mult(translate(0.0, 0.5 * UPPER_ARM_HEIGHT, 0.0), s);
-	var t = mult(modelViewMatrix, instanceMatrix);
-	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
-	gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
-}
-
-// ----------------------------------------------------------------------------
-
-function lowerArm() {
-	var s = scale4(LOWER_ARM_WIDTH, LOWER_ARM_HEIGHT, LOWER_ARM_WIDTH);
-	var instanceMatrix = mult(translate(0.0, 0.5 * LOWER_ARM_HEIGHT, 0.0), s);
-	var t = mult(modelViewMatrix, instanceMatrix);
-	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
-	gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
-}
-
-// --------------------------------------------------------------------------
-
 function lightBox() {
 	var s = scale4(1.0, 1.0, 1.0);
 	var instanceMatrix = mult(translate(-5.0, 1.0, 1.0), s);
@@ -509,148 +421,54 @@ function lightBox() {
 
 // ----------------------------------------------------------------------------------
 
-var renderHorse = function() {
+var clawData = {
+	posx: 0,
+	posy: 0,
+	extend: 0,
+	baseanglex: 0,
+	baseanglex: 0,
+	clawangle: 0,
+	clawgrip: 0,
+}
+
+var renderClaw = function() {
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	modelViewMatrix = translate(lightPosition[0], lightPosition[1], lightPosition[2]);
 	lightBox();
 
-	var threshold = 15;
-
-	if (stop == 0) {
-		anim = anim + animSign;
-		if (anim > threshold) {
-			animSign = -1;
-		} else if (anim < -threshold) {
-			animSign = 1;
-		}
-		var wiggle = anim;
-
-		var baseViewMatrix = rotate(theta[0], 0, 1, 0);
-		var x = Number(theta[1]) + wiggle;
-		var x2 = Number(theta[1]) + wiggle;
-		modelViewMatrix = rotate(theta[0], 0, 1, 0);
-	} else {
-		anim = anim + animSign;
-		if (anim > threshold) {
-			animSign = 0;
-		} else if (anim < -threshold) {
-			animSign = 0;
-		}
-		var wiggle = anim;
-
-		var baseViewMatrix = rotate(theta[0], 0, 1, 0);
-		var x = Number(theta[1]) + wiggle;
-		var x2 = Number(theta[1]) + wiggle;
-		modelViewMatrix = rotate(theta[0], 0, 1, 0);
-	}
-
-	// base();
-
-	var lowerAngle = 60;
-
+	var baseViewMatrix = rotate(theta[0], 0, 1, 0);
 	var pos;
 
-	// pos = clawMachine.upperArm.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	modelViewMatrix = mult(baseViewMatrix, clawMachine.upperArm.defaultMat());
+	modelViewMatrix = mult(baseViewMatrix, clawMachine.upperArm.calculateMat());
 	drawComponent(clawMachine.upperArm);
 
-	// pos = clawMachine.lowerArm.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	modelViewMatrix = mult(baseViewMatrix, clawMachine.lowerArm.defaultMat());
+	modelViewMatrix = mult(baseViewMatrix, clawMachine.lowerArm.calculateMat());
 	drawComponent(clawMachine.lowerArm);
 
-	// pos = clawMachine.clawBase.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	modelViewMatrix = mult(baseViewMatrix, clawMachine.clawBase.defaultMat());
+	modelViewMatrix = mult(baseViewMatrix, clawMachine.clawBase.calculateMat());
 	drawComponent(clawMachine.clawBase);
 
-	// pos = clawMachine.upperClaw1.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	modelViewMatrix = mult(baseViewMatrix, clawMachine.upperClaw1.defaultMat());
+	clawMachine.upperClaw1.offsetMat = mult(clawMachine.upperClaw1.defaultMat, rotate(clawData.clawangle, 0, 0, 90))
+	modelViewMatrix = mult(baseViewMatrix, clawMachine.upperClaw1.calculateMat());
 	drawComponent(clawMachine.upperClaw1);
 
-	// pos = clawMachine.lowerClaw1.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(60, 0, 0, 90));
-	modelViewMatrix = mult(baseViewMatrix, clawMachine.lowerClaw1.defaultMat());
+	modelViewMatrix = mult(baseViewMatrix, clawMachine.lowerClaw1.calculateMat());
 	drawComponent(clawMachine.lowerClaw1);
 
-	// pos = clawMachine.upperClaw2.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(60, 0, 90, 0));
-	modelViewMatrix = mult(baseViewMatrix, clawMachine.upperClaw2.defaultMat());
+	clawMachine.upperClaw2.offsetMat = mult(clawMachine.upperClaw2.defaultMat, rotate(clawData.clawangle, 0, 0, 90))
+	modelViewMatrix = mult(baseViewMatrix, clawMachine.upperClaw2.calculateMat());
 	drawComponent(clawMachine.upperClaw2);
 
-	// pos = clawMachine.lowerClaw2.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(60, 0, 90, 0));
-	modelViewMatrix = mult(baseViewMatrix, clawMachine.lowerClaw2.defaultMat());
+	modelViewMatrix = mult(baseViewMatrix, clawMachine.lowerClaw2.calculateMat());
 	drawComponent(clawMachine.lowerClaw2);
 
-	// pos = clawMachine.upperClaw3.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(-60, 0, 90, 0));
-	modelViewMatrix = mult(baseViewMatrix, clawMachine.upperClaw3.defaultMat());
+	clawMachine.upperClaw3.offsetMat = mult(clawMachine.upperClaw3.defaultMat, rotate(clawData.clawangle, 0, 0, 90))
+	modelViewMatrix = mult(baseViewMatrix, clawMachine.upperClaw3.calculateMat());
 	drawComponent(clawMachine.upperClaw3);
 
-	// pos = clawMachine.lowerClaw3.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(60, 0, 90, 0));
-	modelViewMatrix = mult(baseViewMatrix, clawMachine.lowerClaw3.defaultMat());
+	modelViewMatrix = mult(baseViewMatrix, clawMachine.lowerClaw3.calculateMat());
 	drawComponent(clawMachine.lowerClaw3);
-
-	// pos = clawMachine.lowerClaw.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(60, 0, 1, 0))
-	// drawLowerClaw();
-
-	// pos = clawMachine.lowerClaw.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(60, 0, 0, 0))
-	// drawLowerClaw();
-	//
-	// pos = clawMachine.lowerClaw.calcPos();
-	// modelViewMatrix = mult(baseViewMatrix, translate(pos[0], pos[1], pos[2]));
-	// drawLowerClaw();
-
-	// //Right Front Leg
-	// modelViewMatrix = mult(baseViewMatrix, translate(horse.base.width / 2, horse.base.height / 2, horse.base.width / 1.5));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(180, 0, 0 + x / 60, 1));
-	// upperArm();
-	//
-	// //Right Front Ankle
-	// modelViewMatrix = mult(modelViewMatrix, translate(0.0, horse.upperArm.height - 2, 0.0));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(x2 + 15, -90, 0, 1));
-	// lowerArm();
-	//
-	// //right back
-	// modelViewMatrix = mult(baseViewMatrix, translate(horse.base.width / 2, horse.base.height / 2, -horse.base.width / 1.5));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(180, 0, 0 + x / 60, 1));
-	// upperArm();
-	//
-	// modelViewMatrix = mult(modelViewMatrix, translate(0.0, horse.upperArm.height - 2, 0.0));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(x + 15, -90, 0, 1));
-	// lowerArm();
-	//
-	// //left front
-	// modelViewMatrix = mult(baseViewMatrix, translate(-horse.base.width / 2, horse.base.height / 2, horse.base.width / 1.5));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(180, 0, 0 - x / 60, 1));
-	// upperArm();
-	//
-	// modelViewMatrix = mult(modelViewMatrix, translate(0.0, horse.upperArm.height - 2, 0.0));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(x - 15, +90, 0, 1));
-	// lowerArm();
-	//
-	// //left back
-	// modelViewMatrix = mult(baseViewMatrix, translate(-horse.base.width / 2, horse.base.height / 2, -horse.base.width / 1.5));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(180, 0, 0 - x / 60, 1));
-	// upperArm();
-	//
-	// modelViewMatrix = mult(modelViewMatrix, translate(0.0, horse.upperArm.height - 2, 0.0));
-	// modelViewMatrix = mult(modelViewMatrix, rotate(x - 15, +90, 0, 1));
-	// lowerArm();
 };
 
 // ----------------------------------------------------------------------------
